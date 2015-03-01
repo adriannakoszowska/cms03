b6241c08e00f818598709eabbad4634e3499fe5a
Adrianna <adriannakoszowska@gmail.com>
2015-03-01 14:15:08 +0100

    success ImpressPage CMS

--- config.php
+++ config.php
@@ -1,4 +1,21 @@
 <?php
 
- if(!isset($_GET['install']))
-    header("location: install/?install=1");
+/**
+ * @package ImpressPages
+ */
+
+ return array(
+    'sessionName' => 'ses988515453', // prevents session conflict when two sites runs on the same server
+    'developmentEnvironment' => 1, // displays error and debug information. Change to 0 before deployment to production server
+    'showErrors' => 1, // 0 if you don't wish to display errors on the page
+    'debugMode' => 0, // Debug mode loads raw unminified JavaScript files, alerts AJAX errors.
+    'timezone' => 'Europe/Warsaw', // PHP 5 requires timezone to be set.
+    'db' => array (
+        'hostname' => 'localhost',
+        'username' => 'Kotek',
+        'password' => 'kotek123',
+        'tablePrefix' => 'ip_',
+        'database' => 'cms_03',
+        'charset' => 'utf8',
+      ), // Database configuration
+);
